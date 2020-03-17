71834b258ef589e56c1053c92721d2239b991602
aylwin-rt <61852890+aylwin-rt@users.noreply.github.com>
2020-03-17 17:51:12 -0500

    Se creó un SP_insertar_usuario

--- YoSeEso.sql
+++ YoSeEso.sql
@@ -17,8 +17,7 @@ GO
  CREATE TABLE Pregunta(
 	idPregunta INT IDENTITY(1,1) NOT NULL,
 	Pregunta VARCHAR(200) NOT NULL,
-	idImagen VARCHAR(MAX) NULL,
-	idUsuario VARCHAR(50) NOT NULL,
+	Imagen VARCHAR(MAX) NULL,
 	CONSTRAINT PK_Pregunta PRIMARY KEY (idPregunta)
  )
  GO
@@ -26,8 +25,7 @@ GO
  CREATE TABLE Respuesta(
 	idRespuesta INT IDENTITY(1,1) NOT NULL,
 	Respuesta VARCHAR (200) NOT NULL,
-	IdImagen VARCHAR(MAX) NULL,
-	idUsuario VARCHAR(50) NOT NULL,
+	Imagen VARCHAR(MAX) NULL,
 	CONSTRAINT PK_Respuesta PRIMARY KEY (idRespuesta)
  )
  GO
@@ -66,7 +64,22 @@ INSERT INTO Usuario(IdUsuario,nombres,apellidos,email) VALUES ('LbErT','Albert A
 INSERT INTO Usuario(IdUsuario,nombres,apellidos,email) VALUES ('Dríviliz','Adrian Renato','Rivera Trujillo','ema.renato@gmail.com')
 INSERT INTO Usuario(IdUsuario,nombres,apellidos,email) VALUES ('Tu_Grinkita','Betsi Analy','Llagenta Bernal','tu_llaguenta@gmail.com')
 
-INSERT INTO Pregunta(Pregunta,idImagen,idUsuario) VALUES ('5+5',NULL,'aylwin100')
+
+
+INSERT INTO Pregunta(Pregunta,Imagen) VALUES ('5+5',NULL)
+INSERT INTO Pregunta(Pregunta,Imagen) VALUES ('4x4',NULL)
+INSERT INTO Pregunta(Pregunta,Imagen) VALUES ('5/5',NULL)
+INSERT INTO Pregunta(Pregunta,Imagen) VALUES ('11*11',NULL)
+
+INSERT INTO UsuarioPregunta (idUsuario,idPregunta) VALUES ('aylwin100',1)
+INSERT INTO UsuarioPregunta (idUsuario,idPregunta) VALUES ('aylwin100',2)
+INSERT INTO UsuarioPregunta (idUsuario,idPregunta) VALUES ('LbErt',1)
+INSERT INTO UsuarioPregunta (idUsuario,idPregunta) VALUES ('Dríviliz',4)
+
+--DECLARE @idUsuario VARCHAR(50)
+--SET @idUsuario='aylwin100'
+--SELECT * FROM Usuario INNER JOIN UsuarioPregunta ON Usuario.idUsuario=UsuarioPregunta.idUsuario
+--INNER JOIN Pregunta ON UsuarioPregunta.idPregunta=Pregunta.idPregunta WHERE UsuarioPregunta.idUsuario = @idUsuario
 
 
 --INSERT INTO Pregunta (Pregunta) VALUES ('5+5')
@@ -80,5 +93,18 @@ INSERT INTO Pregunta(Pregunta,idImagen,idUsuario) VALUES ('5+5',NULL,'aylwin100'
 
 --SELECT * FROM Usuario
 --SELECT * FROM Pregunta
+--SELECT * FROM UsuarioPregunta
 
---SELECT * FROM Usuario INNER JOIN UsuarioPregunta ON Usuario.idUsuario=UsuarioPregunta.idUsuario INNER JOIN Pregunta ON UsuarioPregunta.idUsuario=Pregunta.idUsuario
+--SELECT * FROM Usuario INNER JOIN UsuarioPregunta ON Usuario.idUsuario=UsuarioPregunta.idUsuario
+--INNER JOIN Pregunta ON UsuarioPregunta.idPregunta=Pregunta.idPregunta
+
+CREATE PROCEDURE SP_insertar_usuario(
+@idUsuario VARCHAR(50),
+@nombres VARCHAR(100),
+@apellidos VARCHAR(100),
+@email VARCHAR(100))
+AS
+INSERT INTO Usuario (idUsuario,nombres,apellidos,email) VALUES (@idUsuario,@nombres,@apellidos,@email)
+
+--EXEC SP_insertar_usuario 'Japo1202','Jorge Arturo','Pérez Ortiz','jorgearturo1202@hotmail.com'
+
